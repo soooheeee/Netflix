185fa8a07c827f7bdefa79e68d04fe85ca39851e
unknown <tgb0626@naver.com>
2023-08-26 04:10:49 +0900

    UI navbar 생성 및 CSS추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState,useEffect } from "react";
 
-const Nav = () => {
+consfirst = () => {
+
+    const [show, setshow] = useState(false);
+
+    useEffect(() => {
+      window.addEventListener("scroll",()=>{
+        if(window.scroll > 50) {
+          console.log('window.scrollY',window.scrollY);
+          setshow(true);
+        }else{
+          setshow(false);
+        }
+      })
+
+      return () => {
+        window.removeEventListener("scroll",() => {});
+      }
+    }, [third])
+    
   return (
-    <nav>
+    <nav className={`nav${show && "nav_black"}`}>
         <img 
         alt='Netflix logo'
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className='nav_logo'
+        onClick={() => window.location.reload()}
+        />
+                <img 
+        alt='Netflix logo'
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        className='nav_avatar'
         onClick={() => window.location.reload()}
         />
     </nav>
