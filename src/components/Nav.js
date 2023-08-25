95bda9c23c24c81ccd24245050f0ea1549796bab
yn_nub <129060841+soooheeee@users.noreply.github.com>
2023-08-25 22:14:22 +0900

    feat 기능 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
     <nav>
         <img 
         alt='Netflix logo'
-        src="http://www.w3.org/1999/xlink"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className='nav_logo'
         onClick={() => window.location.reload()}
         />
